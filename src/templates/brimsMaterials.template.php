28274259bde4a61588b5e287830b7a5afde7d514
Cabji <catfish@beardeddonkey.com>
2024-03-05 11:10:20 +1000

    update regex

--- src/templates/brimsMaterials.template.php
+++ src/templates/brimsMaterials.template.php
@@ -10,7 +10,7 @@ $tName = "brimsMaterials";
 //              |
 //   this is the regex comment
 
-$aa_template[$tName]["regexes"] = array("/(?# Initial Regex to extract Grid Data)(.*0478 008 608\s)(.*?)(Subtotal.*)/is" => "$2", 
+$aa_template[$tName]["regexes"] = array("/(?# Initial Regex to extract Grid Data)(.*\d{4} \d{3} \d{3}\s)(.*?)(Subtotal.*)/is" => "$2", 
                                         "/(?# Convert Grid Data to CSV Values)(.*?) (.*?)(\s1.00 EACH\s)((\d{0,3}),*(\d{0,3}),*(\d{0,3})(.\d{2}))(.*?\n)/is" => "Materials,Brims Tweed Heads,$1,$2,$5$6$7$8\n"
                                        );
 
